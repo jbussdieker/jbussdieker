13c5015b3fc7d7bcd42c3d0cfc71713061c5d22b
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-08 22:47:35 -0500

    `rails new classy -d mysql`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Classy::Application.config.secret_key_base = 'f11965194cb16b13abcd1f58df1e8497985a9deeaea81dc01193694f34983d8a66bf60d850653f734ce8fb73dce2a7f5b5f3d06f97f488a2b329a1e657ab0fae'
