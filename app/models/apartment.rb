25c491cf61034e30b96e0d97ae86aa3c66dcd0e9
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-02-20 13:04:01 -0800

    Refactor apartment model.

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -5,41 +5,36 @@ class Apartment < ActiveRecord::Base
     self.apartment_tags.collect(&:value).join(", ")
   end
 
+  def self.listing_attributes(listing)
+    {
+      :url => listing.url,
+      :title => listing.title,
+      :longitude => listing.longitude,
+      :latitude => listing.latitude,
+      :price => listing.price,
+      :posted_at => listing.details.posted_at,
+      :neighborhood => listing.nh
+    }
+  end
+
   def self.scrape
     require 'clscrape'
 
     cl = CLscrape::Client.new("sfbay")
-      #["s", 100],
-    listings = cl.get_search("eby", "apa", [
-      ["nh", 48],
-      ["nh", 58],
-      ["nh", 61],
-      ["nh", 62],
-      ["nh", 64],
-      ["nh", 11],
-      ["minAsk", 1000],
-      ["maxAsk", 2200],
-      ["bedrooms", 2],
-    ])
+    listings = cl.get_listings("eby", "apa")
 
     listings.each do |listing|
       apartment = Apartment.find_by(:url => listing.url)
-      options = {
-        :url => listing.url,
-        :title => listing.title,
-        :longitude => listing.longitude,
-        :latitude => listing.latitude,
-        :price => listing.price,
-        :posted_at => listing.details.posted_at
-      }
+
       if apartment
-        apartment.update_attributes(options)
+        apartment.update_attributes(listing_attributes(listing))
       else
-        apartment = Apartment.create(options)
+        apartment = Apartment.create(listing_attributes(listing))
       end
 
-      tags = listing.details.tags.map {|a| {:value => a}}
-      apartment.tags.create(tags)
+      tags = listing.details.tags.map {|tag| {:value => tag}}
+
+      apartment.apartment_tags.create(tags)
     end
   end
 end
